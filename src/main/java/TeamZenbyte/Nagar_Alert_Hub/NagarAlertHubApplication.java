8b4626ed8c1e00c55ef948b46daf4b99c8f72fd0
Neal Chakravarty <nealchakravarty930@gmail.com>
2025-12-21 21:07:57 +0530

    initial commit

---
+++ src/main/java/TeamZenbyte/Nagar_Alert_Hub/NagarAlertHubApplication.java
@@ -0,0 +1,13 @@
+package TeamZenbyte.Nagar_Alert_Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NagarAlertHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NagarAlertHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TeamZenbyte/Nagar_Alert_Hub/NagarAlertHubApplicationTests.java
@@ -0,0 +1,13 @@
+package TeamZenbyte.Nagar_Alert_Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagarAlertHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
